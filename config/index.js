514a9462a82d5b48095721c73ddd4f0691857a5b
seeratawan01 <seeratsdsking@gmail.com>
2021-05-03 21:23:03 +0500

    Added: config.js

---
+++ config/index.js
@@ -0,0 +1,35 @@
+const env = process.env.NODE_ENV || 'development';
+
+const config = {
+    common: {
+        app: {
+            port: parseInt(process.env.APP_PORT) || 3000
+        },
+    },
+    development: {
+        db: {
+            connection: null,
+            host: process.env.DB_HOST,
+            port: parseInt(process.env.DB_PORT),
+            db_name: process.env.DB_DATABASE,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD
+        }
+    },
+    production: {
+        db: {
+            connection: '',
+            host: process.env.DB_HOST,
+            port: parseInt(process.env.DB_PORT),
+            db_name: process.env.DB_DATABASE,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD
+        }
+    }
+}
+
+module.exports = {
+    ...config.common,
+    ...config[env]
+}
+
